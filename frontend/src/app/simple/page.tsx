a7f71bb083650e2bc375020713109ac6ae7690f3
Kishore Chandramouli <kishore7cm@gmail.com>
2025-09-13 22:00:37 -0700

    Add super simple test page for debugging

---
+++ frontend/src/app/simple/page.tsx
@@ -0,0 +1,8 @@
+export default function SimplePage() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Simple Test Page</h1>
+      <p>If you can see this, the basic Next.js is working.</p>
+    </div>
+  )
+}
