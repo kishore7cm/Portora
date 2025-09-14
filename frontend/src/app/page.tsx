d27b1e30add81c42b840d2513977094ec294efd0
Kishore Chandramouli <kishore7cm@gmail.com>
2025-09-13 22:58:06 -0700

    Fix React error #130 - use proper JSX instead of raw HTML

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,9 @@
 export default function Home() {
   return (
-    <html>
-      <head>
-        <title>Portora Test</title>
-      </head>
-      <body>
-        <h1>Hello World!</h1>
-        <p>If you can see this, the basic setup is working.</p>
-        <a href="/simple">Go to Simple Page</a>
-      </body>
-    </html>
+    <div>
+      <h1>Hello World!</h1>
+      <p>If you can see this, the basic setup is working.</p>
+      <a href="/simple">Go to Simple Page</a>
+    </div>
   )
 }
